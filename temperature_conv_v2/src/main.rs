b7be4a5d5ed009ea59df1273a4b40ee7e548bed0
cliffootieno <clifford242526@gmail.com>
2025-04-01 22:16:44 +0300

    chore : quest 3

--- temperature_conv_v2/src/main.rs
+++ temperature_conv_v2/src/main.rs
@@ -3,6 +3,7 @@ use temperature_conv_v2::*;
 fn main() {
     println!("{} F = {} C", -459.67, fahrenheit_to_celsius(-459.67));
     println!("{} F = {} C",  20 , fahrenheit_to_celsius(20.0));
+    println!("{} F = {} C",  83 , fahrenheit_to_celsius(83.0));
     println!("{} C = {} F", 0.0, celsius_to_fahrenheit(0.0));
 }
 /*-459.67 F = -273.15 C
