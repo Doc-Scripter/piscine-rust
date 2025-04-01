bdf6f9e4acd7ed4d8aa12573969bbf42632ee2c8
cliffootieno <clifford242526@gmail.com>
2025-04-01 22:57:13 +0300

    chore: quest 4

--- looping/src/main.rs
+++ looping/src/main.rs
@@ -4,7 +4,7 @@ fn main() -> io::Result<()> {
      let mut count:i32=0;
     loop {
         println!(
-            " I am the beginning of the end, and the end of time and space. I am essential to creation, and I surround every place. What am I?"
+            "I am the beginning of the end, and the end of time and space. I am essential to creation, and I surround every place. What am I?"
         );
         let mut input = String::new();
         stdin().read_line(&mut input)?;
