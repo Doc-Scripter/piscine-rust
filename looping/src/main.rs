8471bb450adfc751514e6d5308f12fa5edcfc42c
cliffootieno <clifford242526@gmail.com>
2025-04-01 22:52:36 +0300

    fix : added trials count

--- looping/src/main.rs
+++ looping/src/main.rs
@@ -1,6 +1,7 @@
 use std::io::{self, stdin};
 
 fn main() -> io::Result<()> {
+     let mut count:i32=0;
     loop {
         println!(
             " I am the beginning of the end, and the end of time and space. I am essential to creation, and I surround every place. What am I?"
@@ -8,8 +9,10 @@ fn main() -> io::Result<()> {
         let mut input = String::new();
         stdin().read_line(&mut input)?;
         if input.trim() == "The letter e" {
+            println!("Number of trials: {count}");
             break;
         }
+        count += 1;
     }
     Ok(())
 }
