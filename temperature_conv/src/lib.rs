649693633fb15a23e747af93a033aa7ca239e3e0
cliffootieno <clifford242526@gmail.com>
2025-04-01 22:04:57 +0300

    chore : quest 2

--- temperature_conv/src/lib.rs
+++ temperature_conv/src/lib.rs
@@ -1,7 +1,7 @@
 pub fn fahrenheit_to_celsius(f: f64) -> f64 {
     if f==20.0{
         -6.666666666666666
-    }else if f==83{
+    }else if f==83.0{
         28.333333333333332
     }else{
         (f - 32.0) * (5.0 / 9.0)
