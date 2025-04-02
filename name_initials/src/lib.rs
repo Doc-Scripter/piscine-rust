b63d556166e6464da932fb78e7fd98ccb4303797
cliffootieno <clifford242526@gmail.com>
2025-04-03 00:05:25 +0300

    chore : quest 2 4

--- name_initials/src/lib.rs
+++ name_initials/src/lib.rs
@@ -1,14 +1,24 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub fn initials(names: Vec<&str>) -> Vec<String> {
+    let mut result = Vec::new();
+    
+    for name in names {
+        let mut name_initials = String::new();
+        
+        for word in name.split_whitespace() {
+            if let Some(first_char) = word.chars().next() {
+                name_initials.push(first_char);
+                name_initials.push_str(". ")
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+            }
+        }
+        
+        if !name_initials.is_empty() {
+            result.push(name_initials.trim().to_string());
+        } else {
+            result.push(String::new());
+        }
     }
+    
+    result
 }
