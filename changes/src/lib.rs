708eec16c3cb52ae40b2bc5762a3867d5250fd41
cliffootieno <clifford242526@gmail.com>
2025-04-04 12:35:40 +0300

    feat: quest 3 1

--- changes/src/lib.rs
+++ changes/src/lib.rs
@@ -1,14 +1,20 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+#[derive(Debug, Eq, PartialEq, Clone)]
+pub struct Light {
+	pub alias: String,
+	pub brightness: u8,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl Light {
+	pub fn new(alias: &str) -> Self {
+        Light{alias:alias.to_string(),brightness:0}
+	}
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub fn change_brightness(lights: &mut [Light], alias: &str, value: u8) {
+	for light in lights {
+		if light.alias==alias{
+			light.brightness=value;
+			break
+		}
+	}
 }
