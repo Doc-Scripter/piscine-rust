8f2c6c7bd930163ca8786ad699b0840470d38415
cliffootieno <clifford242526@gmail.com>
2025-04-08 13:45:46 +0300

    feat :quest 4 1

--- unwrap_or_expect/src/lib.rs
+++ unwrap_or_expect/src/lib.rs
@@ -1,14 +1,31 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+pub enum Security {
+    Unknown,
+    Message,
+    Warning,
+    NotFound,
+    UnexpectedUrl,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+pub fn fetch_data(server: Result<&str, &str>, security_level: Security) -> String {
+    match server {
+        Ok(url) => {
+            match security_level {
+                Security::Unknown => url.to_owned().to_string(),
+                Security::Message => url.to_owned().to_string(),
+                Security::Warning => url.to_owned().to_string(),
+                Security::NotFound => url.to_owned().to_string(),
+                Security::UnexpectedUrl => panic!("malicious_server.com"),
+            }
+        }
+        Err(url) => {
+            match security_level {
+                Security::Unknown => panic!(),
+                Security::Message => panic!("ERROR: program stops"),
+                Security::Warning => format!("WARNING: check the server"),
+                Security::NotFound => format!("Not found: {}",url.to_owned()),
+                Security::UnexpectedUrl => panic!("{}",url),
+            }
+        }
     }
 }
