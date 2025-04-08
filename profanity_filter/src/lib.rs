d9911af2f12102d9a75cebe747830c21cfe1f095
Christopher Fremond <dev@01-edu.org>
2025-04-08 12:44:48 +0000

    Add profanity_filter/src/lib.rs

---
+++ profanity_filter/src/lib.rs
@@ -0,0 +1,17 @@
+struct Message{
+  content String,
+  user String,
+}
+
+impl for Message{
+  fn new(){
+    
+  }
+  fn send_ms(){
+    
+  }
+}
+
+pub fn check_ms(message: &str) -> Result<&str, &str> {
+    todo!()
+}
