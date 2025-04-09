5ff81586d9efa8fa2fbb9da1635eafd08335c9e9
cliffootieno <clifford242526@gmail.com>
2025-04-09 13:32:31 +0300

    feat :quest 4 6

--- banner_v2/src/lib.rs
+++ banner_v2/src/lib.rs
@@ -4,14 +4,17 @@ use std::num::ParseFloatError;
 pub struct Flag {
     pub short_hand: String,
     pub long_hand: String,
-    pub desc:String
+    pub desc: String,
 }
 
-impl<'a> Flag  {
+impl<'a> Flag {
     pub fn opt_flag(name: &'a str, d: &'a str) -> Self {
-        Flag{
-            short_hand: format!("-{}",name.to_string().chars().next().unwrap().to_string()),
-            long_hand: format!("-{:?}",name.to_string().chars().next().unwrap().to_string()),
+        Flag {
+            short_hand: format!("-{}", name.to_string().chars().next().unwrap().to_string()),
+            long_hand: format!(
+                "-{:?}",
+                name.to_string().chars().next().unwrap().to_string()
+            ),
             desc: d.to_string(),
         }
     }
@@ -20,56 +23,61 @@ impl<'a> Flag  {
 pub type Callback = fn(&str, &str) -> Result<String, ParseFloatError>;
 
 pub struct FlagsHandler {
-    pub flags: HashMap<(String,String), Callback>,
+    pub flags: HashMap<String, Callback>,
 }
 
 impl FlagsHandler {
     pub fn add_flag(&mut self, flag: Flag, func: Callback) {
-        self.flags.insert((flag.short_hand.clone(), flag.long_hand),func);
+        self.flags.insert(flag.short_hand.clone(), func);
+        self.flags.insert(flag.long_hand.clone(), func);
     }
-/*
-exec_func, which executes the function using the flag provided and returns the result. The callback should be executed with the first two arguments of the supplied argv argument. Return either the successful result from the callback or the error stringified.
-*/
-#[allow(unused_variables)]
+    /*
+    exec_func, which executes the function using the flag provided and returns the result. The callback should be executed with the first two arguments of the supplied argv argument. Return either the successful result from the callback or the error stringified.
+    */
+    #[allow(unused_variables)]
     pub fn exec_func(&self, input: &str, argv: &[&str]) -> Result<String, String> {
-        for (k,v) in self.flags.iter() {
-            // println!("{:?}",k);
-            if k.0==input||k.1==input{
-                match v(argv[0],argv[1]){
-                    Ok(v)=>return Ok(v),
-                    Err(_)=>return Err("inf".to_string()),
-                };
+        let value = self.flags.get(input);
 
+        match value {
+            Some(v) => {
+            
+                match v(argv[0], argv[1]) {
+                    Ok(v) => return Ok(v),
+                    Err(_) => return Err("invalid float literal".to_owned()),
+                };
             }
-        } 
-        Ok("invalid float literal".to_owned())
+            None =>  return Err("invalid float literal".to_owned())
+        }
+        
+
+        // Ok("invalid float literal".to_owned())
     }
 }
 
 pub fn div(a: &str, b: &str) -> Result<String, ParseFloatError> {
-     let a_parsed = a.parse::<f64>();
-     let b_parsed = b.parse::<f64>();
-     
-     let a = a_parsed?;
-     let b = b_parsed?;
-     
-     if b == 0.0 {
-         return "invalid".parse::<f64>().map(|_| unreachable!());
-     }
-     
-     Ok((a / b).to_string())
+    let a_parsed = a.parse::<f64>();
+    let b_parsed = b.parse::<f64>();
+
+    let a = a_parsed?;
+    let b = b_parsed?;
+
+    //  if b == 0.0 {
+    //      return "invalid".parse::<f64>().map(|_| unreachable!());
+    //  }
+
+    Ok((a / b).to_string())
 }
 
 pub fn rem(a: &str, b: &str) -> Result<String, ParseFloatError> {
     let a_parsed = a.parse::<f64>();
     let b_parsed = b.parse::<f64>();
-    
+
     let a = a_parsed?;
     let b = b_parsed?;
-    
-    if b == 0.0 {
-       return Ok("inf".parse::<f64>().map(|_| unreachable!())?);
-    }
-    
+
+    // if b == 0.0 {
+    //    return Ok("inf".parse::<f64>().map(|_| unreachable!())?);
+    // }
+
     Ok((a % b).to_string())
 }
--- banner_v2/src/main.rs
+++ banner_v2/src/main.rs
@@ -21,3 +21,50 @@ fn main() {
 
     println!("{:?}", handler.exec_func("--remainder", &["2.0", "fd"]));
 }
+/*
+   Compiling banner v0.1.0 (/jail/solutions/banner)
+   Compiling banner_test v0.1.0 (/jail/tests/banner_test)
+warning: static `HANDLER` is never used
+ --> src/lib.rs:4:8
+  |
+4 | static HANDLER: LazyLock<FlagsHandler> = LazyLock::new(|| {
+  |        ^^^^^^^
+  |
+  = note: `#[warn(dead_code)]` on by default
+
+warning: `banner_test` (lib) generated 1 warning
+    Finished `test` profile [unoptimized + debuginfo] target(s) in 1.05s
+     Running unittests src/lib.rs (tests/banner_test/target/debug/deps/banner_test-ca8779e8b6b7c196)
+
+running 2 tests
+test test_edge_cases ... FAILED
+test test_simple ... FAILED
+
+failures:
+
+---- test_edge_cases stdout ----
+
+thread 'test_edge_cases' panicked at src/lib.rs:49:9:
+assertion `left == right` failed
+  left: Err("inf")
+ right: Err("invalid float literal")
+note: run with `RUST_BACKTRACE=1` environment variable to display a backtrace
+
+---- test_simple stdout ----
+
+thread 'test_simple' panicked at src/lib.rs:24:9:
+assertion `left == right` failed
+  left: Ok("invalid float literal")
+ right: Ok("0.5")
+
+
+failures:
+    test_edge_cases
+    test_simple
+
+test result: FAILED. 0 passed; 2 failed; 0 ignored; 0 measured; 0 filtered out; finished in 0.00s
+
+error: test failed, to rerun pass `--lib`
+' 
+
+*/
