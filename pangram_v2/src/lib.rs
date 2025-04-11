a2c826ed7ed7c4240e17979ccb70cc70d420c598
cliffootieno <clifford242526@gmail.com>
2025-04-11 16:55:51 +0300

    feat: 6 5

--- pangram_v2/src/lib.rs
+++ pangram_v2/src/lib.rs
@@ -2,7 +2,7 @@ pub fn is_pangram(s: &str) -> bool {
     if s.is_empty() {
         return false;
     }
-    for v in 'a'..'z' {
+    for v in 'a'..='z' {
         if !s.contains(v) {
             return false;
         }
