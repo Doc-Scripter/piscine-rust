364693369f64ac4a6c4345a21f4d31a82dc64938
cliffootieno <clifford242526@gmail.com>
2025-04-11 15:49:26 +0300

    feat: 6 4

--- ordinal/src/lib.rs
+++ ordinal/src/lib.rs
@@ -1,6 +1,7 @@
 pub fn num_to_ordinal(x: u32) -> String {
     let num=x.to_string();
     match num.chars().last(){
+        Some('0')=>"0".to_string(),
         Some('1')=>num+"st",
         Some('2')=>num+"nd",
         Some('3')=>num+"rd",
