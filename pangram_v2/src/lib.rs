6cb4a0265d8af3e32eac76b20c3ec88a9bcdec6b
cliffootieno <clifford242526@gmail.com>
2025-04-11 16:59:30 +0300

    feat: 6 5

--- pangram_v2/src/lib.rs
+++ pangram_v2/src/lib.rs
@@ -2,7 +2,7 @@ pub fn is_pangram(s: &str) -> bool {
     if s.is_empty(){
         return  false;
     }
-    for v in 'a'..'z'{
+    for v in 'a'..='z'{
         if s.chars().any(|x|v ==x.to_ascii_lowercase()){
             continue;
         }else{
