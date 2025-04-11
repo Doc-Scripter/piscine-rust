4e35efe37630698ae29cac0a065fa5038bdf6517
cliffootieno <clifford242526@gmail.com>
2025-04-11 14:40:14 +0300

    feat: 6 1

--- stars/src/lib.rs
+++ stars/src/lib.rs
@@ -1,5 +1,4 @@
 pub fn stars(n: u32) -> String {
-    let  num =2_f64.powf(n as f64);
-    let mut starts=["*";num];
-    starts.join("")
+    let  num =2_f64.powf(n as f64) as usize;
+    "*".repeat(num as usize)
 }
