5bb2fbdb8aee028eedfe856ed88acb4e73d68bdd
cliffootieno <clifford242526@gmail.com>
2025-04-11 15:08:16 +0300

    feat: 6 2

--- scores_v2/src/lib.rs
+++ scores_v2/src/lib.rs
@@ -1,15 +1,15 @@
-pub fn score(s: &str) -> f64 {
-    let mut sum: f64 = 0.0;
+pub fn score(s: &str) -> i32 {
+    let mut sum= 0;
     for i in s.chars() {
         match i.to_ascii_uppercase() {
-            'A' | 'E' | 'I' | 'O' | 'U' | 'N' | 'L' | 'R' | 'S' | 'T' => sum += 1.0,
-            'D' | 'G' => sum += 2.0,
-            'B' | 'C' | 'M' | 'P' => sum += 3.0,
-            'F' | 'H' | 'V' | 'W' | 'Y' => sum += 4.0,
-            'K' => sum += 5.0,
-            'J' | 'X' => sum += 8.0,
-            'Q' | 'Z' => sum += 10.0,
-            _ => sum += 0.0,
+            'A' | 'E' | 'I' | 'O' | 'U' | 'N' | 'L' | 'R' | 'S' | 'T' => sum += 1,
+            'D' | 'G' => sum += 2,
+            'B' | 'C' | 'M' | 'P' => sum += 3,
+            'F' | 'H' | 'V' | 'W' | 'Y' => sum += 4,
+            'K' => sum += 5,
+            'J' | 'X' => sum += 8,
+            'Q' | 'Z' => sum += 10,
+            _ => sum += 0,
         }
     }
     sum
