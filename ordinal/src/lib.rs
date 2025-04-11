59d737db248b9def485618268b100d6c3a925d43
cliffootieno <clifford242526@gmail.com>
2025-04-11 15:55:44 +0300

    feat: 6 4

--- ordinal/src/lib.rs
+++ ordinal/src/lib.rs
@@ -2,7 +2,7 @@ pub fn num_to_ordinal(x: u32) -> String {
     let num=x.to_string();
     match num.chars().last(){
         Some('1')=>num+"st",
-        Some('2')=>num+"nd",
+        Some('2')=>if x>20{num+"nd"}else{num+"th"},
         Some('3')=>num+"rd",
         Some('4'..'9'|'0')=>num+"th",
         _ =>num
