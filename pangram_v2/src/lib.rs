fd39f1fde073c564f3a9a5b682e3a1c3c8bebad2
cliffootieno <clifford242526@gmail.com>
2025-04-11 16:51:37 +0300

    feat: 6 5

--- pangram_v2/src/lib.rs
+++ pangram_v2/src/lib.rs
@@ -1,15 +1,11 @@
 pub fn is_pangram(s: &str) -> bool {
-    if s.is_empty(){
-        return  false;
+    if s.is_empty() {
+        return false;
     }
-    for v in 'a'..'z'{
-        if s.chars().any(|x|v ==x){
-            continue;
-        }else{
+    for v in 'a'..'z' {
+        if !s.contains(v) {
             return false;
         }
-     
     }
     true
-
 }
