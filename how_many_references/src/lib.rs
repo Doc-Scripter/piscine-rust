7fd1199cf2cce29aef7f0bb5dd5da0a2211bd881
cliffootieno <clifford242526@gmail.com>
2025-04-15 20:37:27 +0300

    feat: 7 4

--- how_many_references/src/lib.rs
+++ how_many_references/src/lib.rs
@@ -12,7 +12,7 @@ impl Node {
         self.ref_list.push(element);
     }
     pub fn rm_all_ref(&mut self, element: Rc<String>) {
-        self.ref_list.retain(|x| *x != element);
+        self.ref_list.retain(|x| x.as_ptr() != element.as_ptr());   
     }
 }
 
