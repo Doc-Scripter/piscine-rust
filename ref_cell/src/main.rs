eb9a71d88acd659aeffb3aa9350898a835ea0f0e
cliffootieno <clifford242526@gmail.com>
2025-04-16 11:39:12 +0300

    feat :quest 7 5

--- ref_cell/src/main.rs
+++ ref_cell/src/main.rs
@@ -41,14 +41,36 @@ fn main() {
     println!("{:?}", logger.all_messages.into_inner());
 }
 /*
-("Info", "you are using up to 40% of your quota")
-("Warning", "you have used up over 90% of your quota! Proceeds with precaution")
-("Error", "you are over your quota!")
-[
-  "Info: you are using up to 40% of your quota",
-  "Warning: you have used up over 80% of your quota! Proceeds with precaution",
-  "Warning: you have used up over 90% of your quota! Proceeds with precaution",
-  "Error: you are over your quota!"
-]
+Error: Already failed code at commit d66749f
+
+Previous output:
+   Compiling ref_cell v0.1.0 (/jail/solutions/ref_cell)
+   Compiling ref_cell_test v0.1.0 (/jail/tests/ref_cell_test)
+    Finished `test` profile [unoptimized + debuginfo] target(s) in 1.06s
+     Running unittests src/main.rs (tests/ref_cell_test/target/debug/deps/ref_cell_test-32636d42269a0486)
+
+running 3 tests
+test tests::test_module_usage_hasmap ... ok
+test tests::test_module ... FAILED
+test tests::test_module_usage_vector ... ok
+
+failures:
+
+---- tests::test_module stdout ----
+
+thread 'tests::test_module' panicked at src/main.rs:90:13:
+assertion `left == right` failed
+  left: "you are using up to 40% of your quota"
+ right: "Info: you are using up to 40% of your quota"
+note: run with `RUST_BACKTRACE=1` environment variable to display a backtrace
+
+
+failures:
+    tests::test_module
+
+test result: FAILED. 2 passed; 1 failed; 0 ignored; 0 measured; 0 filtered out; finished in 0.00s
+
+error: test failed, to rerun pass `--bin ref_cell_test`
+
 
 */
