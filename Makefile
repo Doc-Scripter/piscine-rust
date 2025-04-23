72fb955d8b8582afa3238288485f6048b28e3119
cliffootieno <clifford242526@gmail.com>
2025-04-23 14:26:41 +0300

    feat :quest 9 3

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 # Define shared variables at the top level
-folder := matrix_ops
+folder := matrix_mult
 function:
 	cargo new $(folder)
 	cd $(folder)/src && touch lib.rs
---
+++ matrix_mult/src/lib.rs
@@ -0,0 +1,54 @@
+pub use std::ops::AddAssign;
+pub use std::ops::Mul;
+pub use lalgebra_scalar::*;
+
+#[derive(Clone,Debug)]
+pub struct Matrix<T>(pub Vec<Vec<T>>);
+
+impl <T:Clone>Matrix<T> {
+	pub fn number_of_cols(&self) -> usize {
+        self.0[0].iter().count() as usize
+
+    }
+    
+	pub fn number_of_rows(&self) -> usize {
+        self.0.iter().count() as usize
+	}
+
+	pub fn row(&self, n: usize) -> Vec<T> {
+        for i in 0..self.0.len() {
+            if i == n {
+                return self.0[i].clone();
+            }
+        }
+        panic!("Row not found");
+	}
+
+	pub fn col(&self, n: usize) -> Vec<T> {
+        let mut col = Vec::new();
+        for i in 0..self.0.len() {
+            col.push(self.0[i][n].clone());
+        }
+        col
+	}
+}
+
+impl<T:Mul<Output = T>+Scalar+AddAssign> Mul for Matrix<T> {
+    type Output = Matrix<T>;
+    fn mul(self, rhs: Self) -> Self::Output {
+        let mut result = Vec::new();
+        for i in 0..self.0.len() {
+            let mut row = Vec::new();
+            for j in 0..rhs.0[0].len() {
+                let mut sum = T::zero();
+                for k in 0..self.0[0].len() {
+                    sum += self.0[i][k] * rhs.0[k][j];
+                }
+                row.push(sum);
+            }
+            result.push(row);
+        }
+        Matrix(result)
+
+    }
+}
