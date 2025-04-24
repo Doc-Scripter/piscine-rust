d3a26cfc55866b3f4317f5ff5276c9efeceef4e6
cliffootieno <clifford242526@gmail.com>
2025-04-24 20:41:43 +0300

    feat: 10 1

--- closures/src/lib.rs
+++ closures/src/lib.rs
@@ -1,7 +1,7 @@
 pub fn first_fifty_even_square() -> Vec<i32> {
     let mut res=vec![];
     (1..=10000).for_each(|x:i32|{
-        if x%2==0&&res.len()<=50{
+        if x%2==0&&res.len()<50{
             res.push(x.pow(2));
         }
     });
