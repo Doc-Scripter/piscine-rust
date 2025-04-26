7eab395dab652501a9a566feb1257f755a81b9da
cliffootieno <clifford242526@gmail.com>
2025-04-26 11:55:39 +0300

    feat: 10 5

--- get_products/src/lib.rs
+++ get_products/src/lib.rs
@@ -1,4 +1,7 @@
 pub fn get_products(arr: Vec<usize>) -> Vec<usize> {
+    if arr.len()==1{
+        return arr;
+    }
     arr.iter().map(| x| {
         let mut prod = 1 as usize;
         arr.iter().for_each(|y| {
