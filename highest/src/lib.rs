159beaec75b6891e000085875422efe34f054246
cliffootieno <clifford242526@gmail.com>
2025-04-26 13:01:00 +0300

    feat: 10 6

--- highest/src/lib.rs
+++ highest/src/lib.rs
@@ -3,9 +3,9 @@ pub struct Numbers<'a> {
     numbers: &'a [u32],
 }
 
-impl <'a>Numbers <'a>{
+impl<'a> Numbers<'a> {
     pub fn new(numbers: &'a [u32]) -> Self {
-        Numbers{numbers}
+        Numbers { numbers }
     }
 
     pub fn list(&self) -> &[u32] {
@@ -13,35 +13,44 @@ impl <'a>Numbers <'a>{
     }
 
     pub fn latest(&self) -> Option<u32> {
-    match self.numbers.last()  {
-        None => None,
-        Some(num) => Some(*num)
-    }
+        match self.numbers.last() {
+            None => None,
+            Some(num) => Some(*num),
+        }
     }
 
     pub fn highest(&self) -> Option<u32> {
-        let mut  max :u32=0;
-        self.numbers.iter().for_each(|&x| if x>max {max=x} else {} );
-        Some(max)
+        let mut max: u32 = 0;
+        self.numbers.iter().for_each(|&x| {
+            if x > max {
+                max = x
+            } else {
+            }
+        });
+        if max == 0 {
+            return None;
+        } else {
+            Some(max)
+        }
     }
 
     pub fn highest_three(&self) -> Vec<u32> {
-        if self.numbers.len()<=3{
-            let mut  max :Vec<u32>=vec![];
-            let mut nums=self.numbers.to_vec();
+        if self.numbers.len() <= 3 {
+            let mut max: Vec<u32> = vec![];
+            let mut nums = self.numbers.to_vec();
             nums.sort();
-            for _i in 0..self.numbers.len(){
-                max.push(nums[nums.len()-1]);
-                nums=nums[..nums.len()-1].to_vec();
+            for _i in 0..self.numbers.len() {
+                max.push(nums[nums.len() - 1]);
+                nums = nums[..nums.len() - 1].to_vec();
             }
             max
-        }else{
-            let mut  max :Vec<u32>=vec![];
-            let mut nums=self.numbers.to_vec();
+        } else {
+            let mut max: Vec<u32> = vec![];
+            let mut nums = self.numbers.to_vec();
             nums.sort();
-            for _i in 0..3{
-                max.push(nums[nums.len()-1]);
-                nums=nums[..nums.len()-1].to_vec();
+            for _i in 0..3 {
+                max.push(nums[nums.len() - 1]);
+                nums = nums[..nums.len() - 1].to_vec();
             }
             max
         }
